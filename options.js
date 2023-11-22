49c2b96f6dd256bd0d25f606b47740525e119fc3
Max <max@maxf.net>
2023-11-22 14:31:54 -0500

    updated

---
+++ options.js
@@ -0,0 +1,51 @@
+const debug = require('debug')('dsquares:options');
+
+const _mongoose = require('mongoose');
+_mongoose.set('strictQuery', true);
+
+const MongooseJSProxy = require('@ezsso/mongoose-jsproxy');
+
+require('dotenv').config({ path: '.env.local' });
+
+const options = {
+	'dsquares': {
+		uri: process.env.DSQUARES_MONGODB,
+		options: {
+			useNewUrlParser: true
+		},
+		debug: true
+	}
+};
+
+function mongoose(dbId) {
+	const dbOptions = options[dbId];
+	return async function() {
+		this.connectPromises = {};
+		if(this.connectPromises[dbId]) {
+			// debug('Reusing connection', dbId);
+			return this.connectPromises[dbId];
+		}
+		debug('Creating connection', dbId);
+
+		this._mongoose = new _mongoose.Mongoose();
+		if(dbOptions.debug) {
+			this._mongoose.set('debug', dbOptions.debug);
+		}
+
+		return this.connectPromises[dbId] = this._mongoose.connect(dbOptions.uri, dbOptions.options);
+	}
+}
+
+const dsquaresMongoose = mongoose('dsquares');
+const dsquares	= new MongooseJSProxy({ mongoose: dsquaresMongoose, idField: 'id', options: { collection: 'dsquares' }});
+
+module.exports = {
+	dsquaresMongoose,
+	dsquares,
+	ezsso_oidc_auth_id: process.env.EZSSO_OIDC_AUTH_ID,
+    ezsso_oidc_idp_id: process.env.EZSSO_OIDC_IDP_ID,
+    ezsso_oidc_authorize_uri: process.env.EZSSO_OIDC_AUTHORIZE_URI,
+    ezsso_oidc_redirect_uri: process.env.EZSSO_OIDC_REDIRECT_URI
+
+};
+
---
+++ routes/api.js
@@ -0,0 +1,46 @@
+const debug = require('debug')('dsquares:routes:api');
+
+var express = require('express');
+var router = express.Router();
+
+var dsquares = require('../lib/dsquares');
+
+router.get('/redirect', function(req, res, next) {
+	const { options } = req.app.settings;
+	res.redirect(decodeURIComponent(req.query.url));
+});
+
+router.get('/appinfo', async (req, res) => {
+	const app_info = {
+	};
+
+	// debug(req.id, 'appinfo',app_info);
+	res.json(app_info);
+});
+
+router.get('/squares', async (req, res) => {
+	res.json(await dsquares.listSquares(req));
+});
+
+router.post('/squares', async function(req, res, next) {
+	res.json(await dsquares.createSquare(req));
+});
+
+router.delete('/squares/:square', async function(req, res, next) {
+	res.json(await dsquare.deleteSquare(req));
+});
+
+router.post('/squares/:square', async function(req, res, next) {
+	res.json(await dsquare.createConcern(req));
+});
+
+router.post('/squares/:square/:concern', async function(req, res, next) {
+	res.json(await dsquare.updateConcern(req));
+});
+
+router.delete('/squares/:square/:concern', async function(req, res, next) {
+	res.json(await dsquare.deleteConcern(req));
+});
+
+module.exports = router;
+
