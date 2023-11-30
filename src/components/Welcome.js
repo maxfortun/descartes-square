b3f2a9d655c5c55827fdcf10bcb8406083bf06a7
Max <max@maxf.net>
2023-11-30 18:07:12 -0500

    updated

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,21 @@ import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm'
 import rehypeRaw from "rehype-raw";
 
-import { Box, Button, Grid, FormControl, FormControlLabel, FormLabel, Icon, Paper, Radio, RadioGroup, Switch, Typography } from '@mui/material';
+import {
+	Box,
+	Button,
+	Divider,
+	Grid,
+	FormControl,
+	FormControlLabel,
+	FormLabel,
+	Icon,
+	Paper,
+	Radio,
+	RadioGroup,
+	Switch,
+	Typography
+} from '@mui/material';
 
 const debug = Debug('accounts:Welcome');
 
@@ -17,25 +31,27 @@ export default function () {
 
 	const handleLoginClick = () => setSession(Object.assign({}, session, {login: true}));
 
-	const markdown = `
-Square away your uncertainty with Descartes Squares!  
-Best decisions you'll ever make!  
-You don't have to do it alone - invite others now!  
-`;
-
 	return (
 		<>
-			<ReactMarkdown linkTarget="_blank" children={markdown} remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]} className="Welcome" />
-			<br/>
 			<Box
 				display="flex"
 				justifyContent="center"
 				alignItems="center"
-				flexDirection="column"
+				sx={{ mt: '24px' }}
 			>
-				<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-					<Button className="pulse" color="inherit" variant="contained" onClick={handleLoginClick}>more</Button>
-				</Typography>
+				<Box>
+					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+						Best decision you'll ever make.
+					</Typography>
+				</Box>
+				<Box sx={{ ml: '24px' }}>
+					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+						<Button className="pulse" color="inherit" variant="contained" onClick={handleLoginClick}>more</Button>
+					</Typography>
+				</Box>
+			</Box>
+			<Box sx={{ mt: '24px', mb: '8px' }}><Divider/></Box>
+			<Box>
 			</Box>
 		</>
 	);
