3caeb2d0ecf30c375fafc3c220415a477847f185
Maxim Fortun <max@maxf.net>
2023-12-24 16:48:21 -0500

    updated

--- bin/mongo-init-dsquares.sh
+++ bin/mongo-init-dsquares.sh
@@ -80,6 +80,16 @@ db.authZ.insertMany([
 		rules: [ 
 			{
 				engine: 'jmespathts',
+				expr: 'req.url.pathname == \`/api/invites\`'
+			}
+		]
+	},
+	{
+		authN: '$EZSSO_OIDC_AUTH_ID',
+		id: UUID().toString('hex').match(/^(.{8})(.{4})(.{4})(.{4})(.{12})$/).slice(1,6).join('-'),
+		rules: [ 
+			{
+				engine: 'jmespathts',
 				expr: 'req.url.pathname == \`/api/squares\`'
 			}
 		]
