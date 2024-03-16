12e599c087a84835ebd77d42fe0d61ea9b0e92bf
Max <max@maxf.net>
2024-03-16 16:30:13 +0000

    updated

--- app/lib/ai.js
+++ app/lib/ai.js
@@ -9,9 +9,28 @@ const dsquares = require('./dsquares');
 
 const tokens = {};
 
+const _prompt = (square) => {
+	return `
+		Respond in JSON.
+		Each question with its respective answer in its own array element.
+		List 5 answers per question.
+		Do not include negations of previously given answers.
+		Phrase answers from first person perspective.
+		Add a sentiment to every answer.
+	
+		What will happen if I do ${square.decision}?
+		What will happen if I do not ${square.decision}?
+		What will not happen if I do ${square.decision}?
+		What will not happen if I do not ${square.decision}?
+	`;
+};
+
+
 async function openai(req) {
 	const square = await dsquares.findSquare(req);
-	const prompt = `Respond to each of the following questions in JSON. What will happen if I do ${square.decision}? What will happen if I do not ${square.decision}? What will not happen if I do ${square.decision}? What will not happen if I do not ${square.decision}? Do not include negations of previously given answers.`;
+	const prompt = _prompt(square);
+
+	debug(req.id, 'openai >', prompt);
 
 	const aiRequest = {
 		model: "gpt-3.5-turbo",
@@ -22,8 +41,6 @@ async function openai(req) {
 			}
 		]
 	};
-
-	debug(req.id, 'openai >', prompt);
 
 	const aiResponse = await openAI.chat.completions.create(aiRequest);
 
@@ -42,24 +59,9 @@ async function googleAuth(req) {
 
 async function vertex(req) {
 	const square = await dsquares.findSquare(req);
-	debug(req.id, 'vertex', square);
+	const prompt = _prompt(square);
 
-	const prompt = `
-		Respond in JSON.
-		Each question with its respective answer in its own array element.
-		List 5 answers per question.
-		Do not include negations of previously given answers.
-		Phrase answers from first person perspective.
-		Add a sentiment to every answer.
-
-		What will happen if I do ${square.decision}?
-		What will happen if I do not ${square.decision}?
-		What will not happen if I do ${square.decision}?
-		What will not happen if I do not ${square.decision}?
-
-	`;
-
-	debug(req.id, 'googleai >', prompt);
+	debug(req.id, 'vertex >', prompt);
 
 	const url = `https://${process.env.GOOGLE_LOCATION}-aiplatform.googleapis.com/v1/projects/${process.env.GOOGLE_CLOUD_PROJECT}/locations/${process.env.GOOGLE_LOCATION}/publishers/google/models/${process.env.GOOGLE_AI_MODEL_ID}:predict`;
 
@@ -88,26 +90,26 @@ async function vertex(req) {
 		body: JSON.stringify(aiRequest)
 	};
 
-	debug(req.id, 'googleai >>', url, options);
+	debug(req.id, 'vertex >>', url, options);
 	const fetchResponse = await fetch(url, options)
 	.catch(async e => {
-		debug(req.id, 'googleai !!', e);
+		debug(req.id, 'vertex !!', e);
 		await googleAuth(req);
 		options.headers.Authorization = `Bearer ${tokens.google_access_token}`;
 		return await fetch(url, options);
 	})
 	.then(response => response.json());
 
-	debug(req.id, 'googleai <<', fetchResponse);
+	debug(req.id, 'vertex <<', fetchResponse);
 	let aiResponse = {};
 	try {
 		let textResponse = fetchResponse.predictions[0].content.replaceAll(/^[^{]+/g,'').replaceAll(/[^}]+$/g,'');
-		debug(req.id, 'googleai <<', textResponse);
+		debug(req.id, 'vertex <<', textResponse);
 		aiResponse = JSON.parse(textResponse);
 	} catch(e) {
-		debug(req.id, 'googleai !!', e);
+		debug(req.id, 'vertex !!', e);
 	}
-	debug(req.id, 'googleai <', aiResponse);
+	debug(req.id, 'vertex <', aiResponse);
 
 	return aiResponse;
 }
